1c16d1e0418806feb85e3e7695ba0ba3a094146f
t-rexdex <tyler.dex@gmail.com>
2023-07-24 12:05:46 -0500

    created schemas with signupmodel and corrected auth_routes

--- auth_routes.py
+++ auth_routes.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-auth_router = APIRouter(prefix="/auth")
+auth_router = APIRouter(prefix="/auth", tags=["auth"])
 
 
 @auth_router.get("/")
---
+++ schemas.py
@@ -0,0 +1,12 @@
+from typing import Optional
+
+from pydantic import BaseModel
+
+
+class SignUpModel(BaseModel):
+    id: Optional[int]
+    username: str
+    email: str
+    password: str
+    is_staff: Optional[bool]
+    is_active: Optional[bool]
