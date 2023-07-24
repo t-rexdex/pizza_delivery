4740dfad373554d51f006ed6d00424d31f939b86
t-rexdex <tyler.dex@gmail.com>
2023-07-24 08:47:30 -0500

    added user and order classes to model

--- models.py
+++ models.py
@@ -1,12 +1,42 @@
-from sqlalchemy import Boolean, Column, Integer, String, Text
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Text
+from sqlalchemy.orm import relationship
+from sqlalchemy_utils.types import ChoiceType
 
 from database import Base
 
 
 class User(Base):
     __tablename__ = "user"
-    id = Column(Integer, primary=True)
+    id = Column(Integer, primary_key=True)
     username = Column(String(25), unique=True)
     email = Column(String(80), unique=True)
     password = Column(Text, nullable=True)
     is_staff = Column(Boolean, default=False)
+    is_active = Column(Boolean, default=False)
+    orders= relationship('Order', back_populates='user')
+
+    def __repr__(self):
+        return f"<User {self.username}"
+
+
+class Order(Base):
+    ORDER_STATUSES = (
+        ("PENDING", "pending"),
+        ("IN-TRANSIT", "in-transit"),
+        ("DELIVERED", "delivered"),
+    ) 
+
+    PIZZA_SIZES = (
+        ("SMALL", "small"),
+        ("MEDIUM", "medium"),
+        ("LARGE", "large"),
+        ("EXTRA-LARGE", "extra-large"),
+    )
+
+    __tablename__ = "orders"
+    id = Column(Integer, primary_key=True)
+    quantity = Column(Integer, nullable=False)
+    order_status = Column(ChoiceType(choices=ORDER_STATUSES), default="PENDING")
+    pizza_size = Column(ChoiceType(choices=PIZZA_SIZES), default="SMALL")
+    flavour=
+    user_id=Column(Integer, ForeignKey('user.id'))
